1e95f3773afdd308fd31e5360a55ca85b86bb902
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-07-01 00:16:14 +0200

    Did #29, probably #38 and #26 over the past few commits.

---
+++ src/io/github/norbipeti/thebuttonmcchat/commands/ucmds/admin/DebugCommand.java
@@ -0,0 +1,30 @@
+package io.github.norbipeti.thebuttonmcchat.commands.ucmds.admin;
+
+import org.bukkit.command.CommandSender;
+
+public class DebugCommand extends AdminCommandBase {
+	private static boolean DebugMode = false;
+
+	@Override
+	public String[] GetHelpText(String alias) {
+		return new String[] { "§6---- Debug mode ----",
+				"Toggles debug mode, which prints debug messages to the console." };
+	}
+
+	@Override
+	public String GetAdminCommandPath() {
+		return "debug";
+	}
+
+	@Override
+	public boolean OnCommand(CommandSender sender, String alias, String[] args) {
+		sender.sendMessage("§eDebug mode "
+				+ ((DebugMode = !DebugMode) ? "§aenabled." : "§cdisabled."));
+		return true;
+	}
+
+	public static void SendDebugMessage(String message) {
+		if (DebugMode)
+			System.out.println(message);
+	}
+}
