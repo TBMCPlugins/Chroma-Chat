bf1a5562a091be4420e71fcf857c36f90cb36729
NorbiPeti <szatmari.norbert.peter@gmail.com>
2018-06-15 20:03:23 +0200

    Fix update dir

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ if [ $1 = 'production' ]; then
 echo Production mode
 echo $UPLOAD_KEY > upload_key
 chmod 400 upload_key
-yes | scp -B  -i upload_key -o StrictHostKeyChecking=no $FILENAME travis@server.figytuna.com:/minecraft/main/plugins
+yes | scp -B  -i upload_key -o StrictHostKeyChecking=no $FILENAME travis@server.figytuna.com:/minecraft/main/TBMC/pluginupdates
 fi
