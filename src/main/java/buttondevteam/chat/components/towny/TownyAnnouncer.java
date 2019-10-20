cc6c477add9566d7060dd063317d7fe23d1f9336
NorbiPeti <szatmari.norbert.peter@gmail.com>
2019-10-20 02:29:54 +0200

    Fixed TownyAnnouncer
    It took way too long to figure out how to add an appender that actually takes effect
    Also changed Towny dep, it'll likely break Travis builds

--- src/main/java/buttondevteam/chat/components/towny/TownyAnnouncer.java
+++ src/main/java/buttondevteam/chat/components/towny/TownyAnnouncer.java
@@ -7,10 +7,10 @@ import buttondevteam.lib.chat.TBMCChatAPI;
 import lombok.val;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.filter.LevelRangeFilter;
 import org.apache.logging.log4j.core.layout.PatternLayout;
@@ -19,13 +19,14 @@ import java.util.regex.Pattern;
 
 public class TownyAnnouncer {
 	private static final Pattern LOG_TYPE_PATTERN = Pattern.compile("\\[(\\w+) (?:Msg|Message)](?: (\\w+):)?");
-	private static final Appender HANDLER = new AbstractAppender(TownyAnnouncer.class.getSimpleName(),
-		LevelRangeFilter.createFilter(Level.INFO, Level.INFO, Filter.Result.ACCEPT, Filter.Result.DENY),
+	private static final String APPENDER_NAME = "Chroma";
+	private static final AbstractAppender HANDLER = new AbstractAppender(APPENDER_NAME,
+		LevelRangeFilter.createFilter(Level.INFO, Level.INFO, Filter.Result.ACCEPT, Filter.Result.ACCEPT),
 		PatternLayout.createDefaultLayout()) {
 		@Override
 		public void append(LogEvent logRecord) {
 			if (logRecord.getMessage() == null) return;
-			String message = logRecord.getMessage().toString();
+			String message = logRecord.getMessage().getFormattedMessage();
 			val m = LOG_TYPE_PATTERN.matcher(message);
 			if (!m.find()) return;
 			String groupID = m.group(2); //The group ID is correctly cased
@@ -59,7 +60,23 @@ public class TownyAnnouncer {
 		target = TBMCSystemChatEvent.BroadcastTarget.add("towny");
 		TownyAnnouncer.townChannel = townChannel;
 		TownyAnnouncer.nationChannel = nationChannel;
+		/*System.out.println(LogManager.getLogger("com.palmergames.bukkit.towny"));
 		((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getContext().getConfiguration().addAppender(HANDLER);
+		System.out.println(((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getAppenders());
+		((LoggerContext)LogManager.getContext(false)).updateLoggers();
+		System.out.println(((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getAppenders());
+		((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getContext().updateLoggers();
+		System.out.println(((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getAppenders());*/
+		val lc = (LoggerContext) LogManager.getContext(false);
+		HANDLER.start();
+		lc.getConfiguration().addAppender(HANDLER);
+		Logger logger = lc.getLogger("com.palmergames.bukkit.towny");
+		//System.out.println(logger);
+		//System.out.println(lc.getConfiguration().<Appender>getAppender(HANDLER.getName())); //T defaults to String because of the context which results in a cast exception
+		logger.addAppender(lc.getConfiguration().getAppender(HANDLER.getName()));
+		logger.get().addAppender(HANDLER, Level.INFO, HANDLER.getFilter());
+		lc.updateLoggers();
+		//System.out.println(logger.getAppenders());
 	}
 
 	public static void setdown() {
@@ -67,6 +84,6 @@ public class TownyAnnouncer {
 		target = null;
 		TownyAnnouncer.townChannel = null;
 		TownyAnnouncer.nationChannel = null;
-		((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getContext().getConfiguration().getAppenders().remove(TownyAnnouncer.class.getSimpleName());
+		((Logger) LogManager.getLogger("com.palmergames.bukkit.towny")).getAppenders().remove(APPENDER_NAME);
 	}
 }
